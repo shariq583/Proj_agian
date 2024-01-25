f947d37aa210068bf26d2c91b6b7b72334b3e474
shariq583 <gazdarshariq0@gmail.com>
2024-01-25 19:14:53 +0500

    Dynamic_Constructor.cpp updated!

--- Cpp_Code/Classes/tempCodeRunnerFile.cpp
+++ Cpp_Code/Classes/tempCodeRunnerFile.cpp
@@ -1,5 +1,10 @@
-    // Complex(int y)
-    // {
-    //     a = 0;
-    //     b = y;
-    // }
+  BankDep (int p , int y ,int r)
+    {
+        pre = p;
+        years = y;
+        intr_rte = (r/100);
+        res_val = pre;
+         for (int i = 0; i < y; i++)
+        {
+            res_val = ((p * r) + p);
+        }
