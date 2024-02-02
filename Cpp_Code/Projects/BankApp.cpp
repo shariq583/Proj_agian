d5fbc3fdea9f902634a4650d1cb747ccb22ea32f
shariq583 <gazdarshariq0@gmail.com>
2024-02-02 19:17:56 +0500

    BankApp S&L Pages created!

--- Cpp_Code/Projects/BankApp.cpp
+++ Cpp_Code/Projects/BankApp.cpp
@@ -1,32 +1,91 @@
 #include <iostream>
 using namespace std;
+class AdminDash;
 class cred
 {
 public:
     string userPin;
     string adminPin;
-    string 
+    string userAccNo;
+    string adminAccNo;
     cred()
     {
         userPin = "0000";
         adminPin = "0000";
     }
-    void setUserCred(string inpUserPin)
+
+    void setUserAccNo(string inpUserAccNo)
+    {
+        userAccNo = inpUserAccNo;
+    }
+    void setUserPin(string inpUserPin)
     {
         userPin = inpUserPin;
     }
-    void setAdminCred(string inpAdminPin)
+    void setAdminAccNo(string inpAdminAccNo)
+    {
+        adminPin = inpAdminAccNo;
+    }
+    void setAdminPin(string inpAdminPin)
     {
         adminPin = inpAdminPin;
     }
+    int UserCheckAccNo (string icheckNo)
+    {
+        if (userAccNo == icheckNo)
+        {
+            return 1;
+        }
+        else 
+        {
+            return 0;
+        }
+        
+    }
+    int UserCheckPin (string icheckPin)
+    {
+           if (userAccNo == icheckPin)
+        {
+            return 1;
+        }
+        else 
+        {
+            return 0;
+        }
+    }
+};
+class AdminDash : public cred 
+{
+    public :
+
+};
+class UserInfo : public cred 
+{
+    public :
+    
+
+
 };
 
 int main()
 {
     // Variables and objects 
+    // Choice Vars
     int Mchoice;
     int Uchoice;
+    int Achoice;
+    //User Vars
+    string UPin_S;
+    string UAccNo_S;
+    string UPin_L;
+    string UAccNo_L;
     cred Ucred;
+    // Admin Vars
+    string APin_S;
+    string AAccNo_S;
+    string APin_L;
+    string AAccNo_L;
+    cred Acred;
 
     std::cout << "=========" << std::endl;
     std::cout << "Bank App" << std::endl;
@@ -39,20 +98,82 @@ int main()
 
     switch (Mchoice)
     {
-    case 1:
+    case 1: //User Page
         std::cout << "1.Sign Up!" << std::endl;
         std::cout << "2.Log In!" << std::endl;
         cin >> Uchoice;
         switch (Uchoice)
             {
-            case 1:
-                
+            case 1: //Sign Up
+                cout<<"Enter A Account No.";                
+                cin>>UAccNo_S;
+                Ucred.setUserAccNo(UAccNo_S);
+                cout<<"Set A Account Pin";
+                cin>>UPin_S;
+                Ucred.setUserPin(UPin_S);
                 break;
+            
+            case 2: //Login Page
+            {
+                int tempPL , tempNL;
+                std::cout << "Enter Your account number" << std::endl;
+                cin>>UAccNo_L;
+                tempNL=Ucred.UserCheckAccNo(UAccNo_L);
+                std::cout << "Enter Your account Pin" << std::endl;
+                cin>>UPin_L;
+                tempPL=Ucred.UserCheckAccNo(UPin_L);
+                if (tempPL == 0)
+                {
+                    // code to be put.
+                }
+                else
+                {
+                    /* code */
+                }
+                
+            }
 
             default: //UChoice
+                std::cout << "Wrong Choice!" << std::endl;
                 break;
-            }
+             }
             break;
+    case 2: // Admin Page
+    {
+        std::cout << "1.Sign Up!" << std::endl;
+        std::cout << "2.Log In!" << std::endl;
+        cin >> Achoice;
+        switch (Achoice)
+            {
+            case 1://Sign up!
+                cout<<"Enter A Account No.";                
+                cin>>AAccNo_S;
+                Acred.setAdminAccNo(AAccNo_S);
+                cout<<"Set A Account Pin";
+                cin>>APin_S;
+                Acred.setUserPin(APin_S);
+                break;
+            
+            case 2://Login 
+            {
+                int tempPLA , tempNLA;
+                std::cout << "Enter Your account number" << std::endl;
+                cin>>AAccNo_L;
+                tempNLA=Acred.UserCheckAccNo(AAccNo_L);
+                std::cout << "Enter Your account Pin" << std::endl;
+                cin>>APin_L;
+                tempPLA=Acred.UserCheckAccNo(APin_L);
+                if (tempPLA == 0)
+                {
+                    // code to be put.
+                }
+                else
+                {
+                    /* code */
+                }
+                
+            }
+    }
 
     default: //Mchoice
         break;
