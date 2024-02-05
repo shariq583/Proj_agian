012add827b7785ad82f734513cd016760bdf5255
shariq583 <gazdarshariq0@gmail.com>
2024-02-05 22:43:17 +0500

    TW update !

---
+++ Cpp_Code/Classes/Multiple_Inheritence.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/* 
+  Multiple Inheritance:
+    IT IS A CHILD CLASS CONTAINING MORE THAN ONE PARENT CLASS.
+*/
+
+
+
+int main() {
+ 
+    return 0;
+}
+
+
+
+
+
