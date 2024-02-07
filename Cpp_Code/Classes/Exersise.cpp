d21391ae168abb8f6ea3c24c0425a438295e0cb8
shariq583 <gazdarshariq0@gmail.com>
2024-02-07 16:40:31 +0500

    Exersise update

--- Cpp_Code/Classes/Exersise.cpp
+++ Cpp_Code/Classes/Exersise.cpp
@@ -4,12 +4,11 @@ Exersise Questions :
     */
 // Answer :
 #include<iostream>
+#include<cmath>
 using namespace std;
 class SimpleCal
 {
-    protected :
-    int num1_P;
-    int num2_P;
+    public :
     SimpleCal()
     {
         num1_P=0;
@@ -20,7 +19,11 @@ class SimpleCal
         num1_P=a;
         num2_P=b;
     }
-    int SimCal (string ch){
+    protected :
+    float num1_P;
+    float num2_P;
+    public :
+    float SimCal (string ch){
     if (ch=="+")
     {
         return num1_P+num2_P;
@@ -29,15 +32,63 @@ class SimpleCal
     {
         return num1_P-num2_P;
     }
-
+    else if (ch=="*")
+    {
+        return num1_P*num2_P;
     }
-     
+    else if (ch=="/")
+    {
+        return num1_P/num2_P;
+    }
+    else 
+    {
+        return 0;
+    }
+    }
+};
+class SciCal
+{
+public :
+    protected :
+    float num1_S;
+    float num2_S;
+    public :
+    SciCal()
+    {
+        num1_S=0;
+        num2_S=0;
+    }
+    SciCal(int a )
+    {
+        num1_S=a;
+    }
+    public :
+    float SfiCal (string chs){
+    if (chs=="cos" || chs=="Cos" || chs=="COS")
+    {
+        return (cos(num1_S));
+    }
+    else if (chs=="sin" || chs=="Sin" || chs=="SIN")
+    {
+       return (sin(num1_S));
+    }
+    else if (chs=="tan" || chs=="Tan" || chs=="TAN")
+    {
+       return (tan(num1_S));
+
+    }    
+    else 
+    {
+        return 0;
+    }
+    }
 
 };
 
 
-
 int main()
 {
+    SciCal a(90);    
+    cout<< a.SfiCal("sin");
 
 }
