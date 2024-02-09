f8d26296b74d7c172f71b31f47c885d7577eb7ac
shariq583 <gazdarshariq0@gmail.com>
2024-02-09 12:16:47 +0500

    Js merge!!

---
+++ Web_development/Html,Css,Js/JavaScript/Chapter 1/java.js
@@ -0,0 +1,30 @@
+// // Varibale declaration
+// var num = 1;
+// var num2 = 2;
+// console.log(num+num2)
+// // array in js 
+// var arr = [1,2,3,4,5]
+// console.log(arr)
+// function avg(a, b)
+// {
+    //     c=(a/b);
+    //     return c;
+    // }
+    // console.log(avg(5 , 8));
+    // function RCheck(age) {
+    //     if (age>=18)
+    //         {
+    //             alert("You are good to vote!")
+    //         }
+    //         else if ( age>0 )
+    //         {
+    //             alert("You Can't vote ")                
+    //         }
+    //         else  ( age<=0 )
+    //         {
+    //             alert("Lol you are not born yet!")                
+    //         }
+    // }
+
+
+console.clear;
