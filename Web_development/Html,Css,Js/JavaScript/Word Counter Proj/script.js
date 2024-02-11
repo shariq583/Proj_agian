adcf13d4367d03eed5de466fe443014ba70f47de
shariq583 <gazdarshariq0@gmail.com>
2024-02-11 15:53:08 +0500

    Word counter done!

--- Web_development/Html,Css,Js/JavaScript/Word Counter Proj/script.js
+++ Web_development/Html,Css,Js/JavaScript/Word Counter Proj/script.js
@@ -1,5 +1,9 @@
 
 function AskInp() {
-    let inp = document.getElementById('TxtInput');
-    console.log(inp.value);
+    let inp = document.getElementById('InpTxt');
+    let inptext = inp.value;
+    let tPCount = inptext.replace(/\s+/g, '');
+    let inpcount = tPCount.length;
+    let precount =document.getElementById('count');
+    precount.innerText = inpcount;
 }
