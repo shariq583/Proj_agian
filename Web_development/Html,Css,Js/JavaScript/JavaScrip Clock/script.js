dd3664fa1483f4fd30d1c3ac86a738ca0f483a0e
shariq583 <gazdarshariq0@gmail.com>
2024-03-13 22:02:24 +0500

    .

--- Web_development/Html,Css,Js/JavaScript/JavaScrip Clock/script.js
+++
@@ -1,9 +0,0 @@
-let date;
-let time;
-let defDate = document.getElementById('deftime')
-setInterval(() => {
-    date =  new Date();
-    time= date.getHours()+":"+ date.getMinutes()+":"+date.getSeconds();
-    day= date.toLocaleDateString();
-    defDate.innerText=time+' on '+day    
-}, 1000);
